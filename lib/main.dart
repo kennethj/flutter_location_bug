7a6339a1616a0cad7b79e4820637b347836e0b9f
Kenneth Jones <github@kennethdjones.com>
2022-10-06 10:43:43 -0500

    Clean up example code

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,17 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Location bug',
-      theme: ThemeData(),
-      home: const MyHomePage(),
-    );
-  }
+  Widget build(context) => MaterialApp(title: 'Location bug', theme: ThemeData(), home: const MyHomePage());
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(
@@ -37,10 +23,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: Location().requestPermission,
                 child: const Text("Request location Permission"),
               ),
-              const Text("""Reproducation steps:
+              const Text("""Reproduction steps:
+
 1. Click the "Request location Permission" button to request location permission
 2. Select "Only this time" on the permission dialog
-3. Put app in background for 60 seconds to allow the permission to expire
+3. Put the app in the background for 60 seconds to allow the permission to expire
 4. Return to app
 """),
             ],
